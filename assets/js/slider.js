6dcead449f941a317901f3e7b2b393cfcdd250e8
Ljilja <ljiljana.tarabaric@gmail.com>
2018-10-19 21:46:18 +0200

    Updates

---
+++ assets/js/slider.js
@@ -0,0 +1,30 @@
+/* slider w3 */
+
+var slideIndex = 0;
+var slideIndexSmall = 0;
+showSlides();
+
+
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName('slide');   
+    
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    
+    slides[slideIndex-1].style.display = "block";  
+    
+    setTimeout(showSlides, 5000); 
+}
+
+
+
+
+
+
+
+
